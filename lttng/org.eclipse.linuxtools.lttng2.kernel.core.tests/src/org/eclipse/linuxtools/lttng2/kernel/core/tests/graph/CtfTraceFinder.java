fe45f9eae8b777f63f18de2f9715c54d1c5c951a
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-05-20 13:50:58 -0400

    Extract CTF utilities
    They will be used in other tests.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>
    
    Conflicts:
    	lttng/org.eclipse.linuxtools.lttng2.kernel.core.tests/src/org/eclipse/linuxtools/lttng2/kernel/core/tests/graph/TestGraphMultiHost.java
    
    Conflicts:
    	lttng/org.eclipse.linuxtools.lttng2.kernel.core.tests/src/org/eclipse/linuxtools/lttng2/kernel/core/tests/graph/TestGraphMultiHost.java
    
    Change-Id: I4883a51cfe7681064bcbbe296d68c6a16da305f3

---
+++ lttng/org.eclipse.linuxtools.lttng2.kernel.core.tests/src/org/eclipse/linuxtools/lttng2/kernel/core/tests/graph/CtfTraceFinder.java
@@ -0,0 +1,119 @@
+package org.eclipse.linuxtools.lttng2.kernel.core.tests.graph;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.linuxtools.lttng2.kernel.core.trace.LttngKernelTrace;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
+import org.eclipse.linuxtools.tmf.core.synchronization.ITmfTimestampTransform;
+import org.eclipse.linuxtools.tmf.core.synchronization.SyncAlgorithmFullyIncremental;
+import org.eclipse.linuxtools.tmf.core.synchronization.SynchronizationAlgorithm;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
+import org.eclipse.linuxtools.tmf.core.trace.TmfExperiment;
+import org.eclipse.linuxtools.tmf.core.trace.TmfTrace;
+
+/**
+ * Find traces under directory
+ *
+ * @author Francis Giraldeau  <francis.giraldeau@gmail.com>
+ */
+public class CtfTraceFinder extends SimpleFileVisitor<Path> {
+    private final PathMatcher matcher;
+    private List<Path> results;
+    CtfTraceFinder() {
+        matcher = FileSystems.getDefault().getPathMatcher("glob:metadata");
+        results = new LinkedList<>();
+    }
+    @Override
+    public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+        Path name = path.getFileName();
+        if (name != null && matcher.matches(name)) {
+            results.add(path.getParent());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    /**
+     * Return results
+     * @return results
+     */
+    public List<Path> getResults() {
+        return results;
+    }
+
+    /**
+     * Make an experiment of CTF traces
+     * @param base base directory of traces
+     * @param traceKlass the trace type
+     * @param evKlass the event type
+     * @return experiment
+     */
+    public static TmfExperiment makeTmfExperiment(String dir, String base, Class<? extends ITmfTrace> traceKlass, Class<? extends ITmfEvent> evKlass) {
+        List<Path> paths = findCtfTrace(Paths.get(dir, base));
+        final ITmfTrace[] traces = new TmfTrace[paths.size()];
+        int i = 0;
+        for (Path p: paths) {
+            ITmfTrace tmp = null;
+            try {
+                tmp = traceKlass.newInstance();
+                tmp.initTrace(null, p.toString(), evKlass);
+            } catch (InstantiationException | IllegalAccessException | TmfTraceException e) {
+                fail(e.getMessage());
+            }
+            traces[i++] = tmp;
+        }
+
+        TmfExperiment experiment = new TmfExperiment();
+        experiment.initExperiment(evKlass, "", traces, Integer.MAX_VALUE, null);
+        SynchronizationAlgorithm algo = new SyncAlgorithmFullyIncremental();
+        try {
+            algo = experiment.synchronizeTraces(true, algo);
+        } catch (TmfTraceException e) {
+            e.printStackTrace();
+        }
+        for (ITmfTrace trace: experiment.getTraces()) {
+            ITmfTimestampTransform tstrans = algo.getTimestampTransform(trace.getHostId());
+            System.out.println(tstrans);
+            trace.setTimestampTransform(tstrans);
+        }
+        return experiment;
+    }
+
+    /**
+     * Make experiment from a trace directory with default trace and event type
+     * @param base trace directory
+     * @return the experiment
+     */
+    public static TmfExperiment makeTmfExperiment(String dir, String base) {
+        return makeTmfExperiment(dir, base, LttngKernelTrace.class, ITmfEvent.class);
+    }
+
+    /**
+     * Utility method to walk directories and find CTF traces
+     *
+     * @param base base directory
+     * @return list of trace paths
+     */
+    public static List<Path> findCtfTrace(Path base) {
+        CtfTraceFinder finder = new CtfTraceFinder();
+        try {
+            Files.walkFileTree(base, finder);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+        return finder.getResults();
+
+    }
+}
--- lttng/org.eclipse.linuxtools.lttng2.kernel.core.tests/src/org/eclipse/linuxtools/lttng2/kernel/core/tests/graph/TestGraphMultiHost.java
+++ lttng/org.eclipse.linuxtools.lttng2.kernel.core.tests/src/org/eclipse/linuxtools/lttng2/kernel/core/tests/graph/TestGraphMultiHost.java
@@ -3,25 +3,15 @@ package org.eclipse.linuxtools.lttng2.kernel.core.tests.graph;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.linuxtools.internal.lttng2.kernel.core.LttngStrings;
 import org.eclipse.linuxtools.internal.lttng2.kernel.core.TcpEventStrings;
 import org.eclipse.linuxtools.internal.lttng2.kernel.core.graph.building.LttngKernelExecGraphProvider;
 import org.eclipse.linuxtools.internal.lttng2.kernel.core.graph.building.LttngKernelExecutionGraph;
-import org.eclipse.linuxtools.lttng2.kernel.core.trace.LttngKernelTrace;
 import org.eclipse.linuxtools.tmf.analysis.graph.core.base.TmfGraph;
 import org.eclipse.linuxtools.tmf.analysis.graph.core.base.TmfVertex;
 import org.eclipse.linuxtools.tmf.analysis.graph.core.building.AbstractTmfGraphProvider;
@@ -33,22 +23,18 @@ import org.eclipse.linuxtools.tmf.analysis.graph.core.model.TmfModelRegistry;
 import org.eclipse.linuxtools.tmf.analysis.graph.core.model.TmfSystemModelWithCpu;
 import org.eclipse.linuxtools.tmf.analysis.graph.core.model.TmfWorker;
 import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
-import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
 import org.eclipse.linuxtools.tmf.core.request.ITmfEventRequest;
 import org.eclipse.linuxtools.tmf.core.request.ITmfEventRequest.ExecutionType;
 import org.eclipse.linuxtools.tmf.core.request.TmfEventRequest;
 import org.eclipse.linuxtools.tmf.core.signal.TmfTraceOpenedSignal;
-import org.eclipse.linuxtools.tmf.core.synchronization.ITmfTimestampTransform;
-import org.eclipse.linuxtools.tmf.core.synchronization.SyncAlgorithmFullyIncremental;
-import org.eclipse.linuxtools.tmf.core.synchronization.SynchronizationAlgorithm;
 import org.eclipse.linuxtools.tmf.core.tests.shared.TmfTestHelper;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
 import org.eclipse.linuxtools.tmf.core.trace.TmfExperiment;
-import org.eclipse.linuxtools.tmf.core.trace.TmfTrace;
-import org.eclipse.linuxtools.tmf.ctf.core.CtfTmfTrace;
 import org.junit.Test;
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 
 /**
  * Class to test multiple host support for graph
@@ -79,107 +65,12 @@ public class TestGraphMultiHost {
 
     private static String TRACE_DIR = "traces";
 
-    private static class CtfTraceFinder extends SimpleFileVisitor<Path> {
-        private final PathMatcher matcher;
-        private List<Path> results;
-
-        CtfTraceFinder() {
-            matcher = FileSystems.getDefault().getPathMatcher("glob:metadata");
-            results = new LinkedList<>();
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
-            Path name = path.getFileName();
-            if (name != null && matcher.matches(name)) {
-                results.add(path.getParent());
-            }
-            return FileVisitResult.CONTINUE;
-        }
-
-        /**
-         * Return results
-         *
-         * @return results
-         */
-        public List<Path> getResults() {
-            return results;
-        }
-    }
-
-    private static List<Path> findCtfTrace(Path base) {
-        CtfTraceFinder finder = new CtfTraceFinder();
-        try {
-            Files.walkFileTree(base, finder);
-        } catch (IOException e) {
-            fail(e.getMessage());
-        }
-        return finder.getResults();
-
-    }
-
-    /**
-     * Make an experiment of CTF traces
-     *
-     * @param base
-     *            base directory of traces
-     * @param traceKlass
-     *            the trace type
-     * @param evKlass
-     *            the event type
-     * @return experiment
-     */
-    public static TmfExperiment makeTmfExperiment(String base, Class<? extends ITmfTrace> traceKlass, Class<? extends ITmfEvent> evKlass) {
-        List<Path> paths = findCtfTrace(Paths.get(TRACE_DIR, base));
-        final ITmfTrace[] traces = new TmfTrace[paths.size()];
-        int i = 0;
-        for (Path p : paths) {
-            ITmfTrace tmp = null;
-            try {
-                tmp = traceKlass.newInstance();
-                tmp.initTrace(null, p.toString(), evKlass);
-            } catch (InstantiationException | IllegalAccessException | TmfTraceException e) {
-                fail(e.getMessage());
-            }
-            traces[i++] = tmp;
-        }
-
-        TmfExperiment experiment = new TmfExperiment();
-        try (CtfTmfTrace bidon = new CtfTmfTrace()) {
-            bidon.getName();
-            experiment.initExperiment(evKlass, "", traces, Integer.MAX_VALUE, null);
-            SynchronizationAlgorithm algo = new SyncAlgorithmFullyIncremental();
-            try {
-                algo = experiment.synchronizeTraces(true, algo);
-            } catch (TmfTraceException e) {
-                e.printStackTrace();
-            }
-            for (ITmfTrace trace : experiment.getTraces()) {
-                ITmfTimestampTransform tstrans = algo.getTimestampTransform(trace.getHostId());
-                System.out.println(tstrans);
-                trace.setTimestampTransform(tstrans);
-            }
-        }
-        return experiment;
-    }
-
-    /**
-     * Make experiment from a trace directory with default trace and event type
-     *
-     * @param base
-     *            trace directory
-     * @return the experiment
-     */
-    public static TmfExperiment makeTmfExperiment(String base) {
-        return makeTmfExperiment(base, LttngKernelTrace.class, ITmfEvent.class);
-    }
-
     /**
      * Test that traces are correctly found
      */
     @Test
     public void testFindCtfTraces() {
-        List<Path> findCtfTrace = findCtfTrace(Paths.get(TRACE_DIR, EXP_PHD_HOG));
+        List<Path> findCtfTrace = CtfTraceFinder.findCtfTrace(Paths.get(TRACE_DIR, EXP_PHD_HOG));
         assertEquals(2, findCtfTrace.size());
     }
 
@@ -188,9 +79,9 @@ public class TestGraphMultiHost {
      */
     @Test
     public void testMakeExperiment() {
-        TmfExperiment experiment = makeTmfExperiment(EXP_PHD_HOG);
+        TmfExperiment experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_PHD_HOG);
         assertEquals(2, experiment.getTraces().length);
-        experiment = makeTmfExperiment(EXP_DJANGO_INDEX);
+        experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_DJANGO_INDEX);
         assertEquals(3, experiment.getTraces().length);
     }
 
@@ -205,7 +96,7 @@ public class TestGraphMultiHost {
      */
     @Test
     public void testTraceSynchronization() throws InterruptedException {
-        TmfExperiment experiment = makeTmfExperiment(EXP_PHD_HOG);
+        TmfExperiment experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_PHD_HOG);
         TmfEventRequest request = new TmfEventRequest(ITmfEvent.class, 0, ITmfEventRequest.ALL_DATA, ExecutionType.BACKGROUND) {
             public long prev = 0;
 
@@ -234,7 +125,7 @@ public class TestGraphMultiHost {
      */
     @Test
     public void testSwapperExistsWithHelper() throws Throwable {
-        TmfExperiment experiment = makeTmfExperiment(EXP_PHD_HOG);
+        TmfExperiment experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_PHD_HOG);
         TmfTraceOpenedSignal signal = new TmfTraceOpenedSignal(this, experiment, null);
         experiment.traceOpened(signal);
 
@@ -259,7 +150,7 @@ public class TestGraphMultiHost {
      */
     @Test
     public void testGraphDjango() throws Throwable {
-        TmfExperiment experiment = makeTmfExperiment(EXP_DJANGO_INDEX);
+        TmfExperiment experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_DJANGO_INDEX);
         TmfTraceOpenedSignal signal = new TmfTraceOpenedSignal(this, experiment, null);
         experiment.traceOpened(signal);
 
@@ -306,7 +197,7 @@ public class TestGraphMultiHost {
      */
     @Test
     public void testWgetTrace() throws Throwable {
-        TmfExperiment experiment = makeTmfExperiment(EXP_WGET);
+        TmfExperiment experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_WGET);
         TmfTraceOpenedSignal signal = new TmfTraceOpenedSignal(this, experiment, null);
         experiment.traceOpened(signal);
 
@@ -336,7 +227,7 @@ public class TestGraphMultiHost {
      */
     @Test
     public void testAssertOutgoingNet() throws Throwable {
-        TmfExperiment experiment = makeTmfExperiment(EXP_DJANGO_INDEX);
+        TmfExperiment experiment = CtfTraceFinder.makeTmfExperiment(TRACE_DIR, EXP_DJANGO_INDEX);
         TmfTraceOpenedSignal signal = new TmfTraceOpenedSignal(this, experiment, null);
         experiment.traceOpened(signal);
 
@@ -390,9 +281,11 @@ public class TestGraphMultiHost {
 
         private AbstractTmfGraphProvider fProvider;
         private TmfSystemModelWithCpu fSystem;
+        private Table<String, Integer, Boolean> schedState;
 
         public CheckTTWU(AbstractTmfGraphProvider provider) {
             super();
+            schedState = HashBasedTable.create();
             fProvider = provider;
             fSystem = provider.getModelRegistry().getModel(TmfSystemModelWithCpu.class);
             System.out.println(fProvider + " " + fSystem); // (void)
@@ -400,10 +293,17 @@ public class TestGraphMultiHost {
 
         @Override
         public void handleEvent(ITmfEvent event) {
-            Long exp = (Long) event.getContent().getField("context._tid").getValue();
-            TmfWorker worker = fSystem.getWorkerCpu(event.getTrace().getHostId(), Integer.parseInt(event.getSource()));
-            Long act = worker.getId();
-            assertEquals(exp, act);
+            String host = event.getTrace().getHostId();
+            Integer cpu = Integer.parseInt(event.getSource());
+            if (!schedState.contains(host, cpu) && event.getType().getName().equals("sched_switch")) {
+                schedState.put(host, cpu, true);
+            }
+            if (schedState.contains(host, cpu)) {
+                Long exp = (Long) event.getContent().getField("context._tid").getValue();
+                TmfWorker worker = fSystem.getWorkerCpu(event.getTrace().getHostId(), Integer.parseInt(event.getSource()));
+                Long act = worker.getId();
+                assertEquals(exp, act);
+            }
         }
 
     }
