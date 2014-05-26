2d6d2225f3b24fed2e03cd1704d143131a4cd5db
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-05-26 13:12:41 -0400

    Cleanup and documentation
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>
    Conflicts:
    	lttng/org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/synchronization/SyncGraphTest.java
    	lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/synchronization/SyncGraph.java
    
    Change-Id: Ia2e63be47ef6456a842bd57935bd0f4f1378cf74

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/synchronization/SyncGraph.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/synchronization/SyncGraph.java
@@ -2,7 +2,12 @@ package org.eclipse.linuxtools.tmf.core.synchronization;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
@@ -15,45 +20,108 @@ import com.google.common.collect.Multimap;
  * list and is directed. To create undirected graph, add the edge in both
  * directions.
  *
- * V is the vertex type and E is the label (weight) type.
- *
  * @author Francis Giraldeau <francis.giraldeau@gmail.com>
+ * @param <V> The vertices type
+ * @param <E> The edge annotation type
+ * @since 3.1
  */
 public class SyncGraph<V, E> {
 
     private Multimap<V, Edge<V, E>> adj;
     private Set<V> vertices;
 
+    /**
+     * Construct empty graph
+     */
     public SyncGraph() {
         adj = ArrayListMultimap.create();
         vertices = new HashSet<>();
     }
 
+    /**
+     * Add edge from v to w and annotation label
+     * @param v from
+     * @param w to
+     * @param label the edge label
+     */
     public void addEdge(V v, V w, E label) {
         adj.put(v, new Edge<>(v, w, label));
         vertices.add(v);
         vertices.add(w);
     }
 
+    /**
+     * Number of edges
+     * @return number of edges
+     */
     public int E() {
         return adj.entries().size();
     }
 
+    /**
+     * Number of vertices
+     * @return number of vertices
+     */
     public int V() {
         return vertices.size();
     }
 
+    /**
+     * Returns the adjacent edges of the given vertex
+     * @param v the vertex
+     * @return the adjacent vertices
+     */
     public Collection<Edge<V, E>> adj(V v) {
         return adj.get(v);
     }
 
-    public Collection<Edge<V, E>> path(V start, V end) {
-        // TODO
-        ArrayList<Edge<V, E>> path = new ArrayList<>();
-        System.out.println(start + " " + end);
+    /**
+     * Returns a path between start and end vertices.
+     *
+     * @param start vertex
+     * @param end vertex
+     * @return the list of edges between start and end vertices
+     */
+    public List<Edge<V, E>> path(V start, V end) {
+        ArrayList<Edge< V, E>> path = new ArrayList<>();
+        HashMap<V, Edge<V, E>> hist = new HashMap<>();
+        HashSet<V> visited = new HashSet<>();
+        Queue<V> queue = new LinkedList<>();
+        queue.offer(start);
+        while(!queue.isEmpty()) {
+            V node = queue.poll();
+            visited.add(node);
+            for (Edge<V, E> e: adj(node)) {
+                V to = e.getTo();
+                if (!visited.contains(to)) {
+                    queue.offer(e.getTo());
+                    if (!hist.containsKey(e.getTo())) {
+                        hist.put(e.getTo(), e);
+                    }
+                }
+            }
+        }
+        V node = end;
+        while(true) {
+            Edge<V, E> edge = hist.get(node);
+            if (edge == null) {
+                break;
+            }
+            path.add(edge);
+            node = edge.getFrom();
+            if (node == start) {
+                break;
+            }
+        }
+        Collections.reverse(path);
         return path;
     }
 
+    /**
+     * Check if this graph is connected (no partitions)
+     *
+     * @return true if the graph is connected, false otherwise
+     */
     public boolean isConnected() {
         HashSet<V> visited = new HashSet<>();
         Stack<V> stack = new Stack<>();
