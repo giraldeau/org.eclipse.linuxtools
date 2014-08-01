e8abb6d55fd72523d6e874c2a5c39be42e66e356
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-08-01 14:48:24 -0400

    Add efficient union find algorithm
    The union find allows to detect when there is a valid synchronization
    between each nodes, when there is only 1 partitions.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/matching/WeightedQuickUnion.java
@@ -0,0 +1,58 @@
+package org.eclipse.linuxtools.tmf.core.event.matching;
+
+
+/**
+ * Source: Algorithms: part I (4th edition)
+ *
+ * @author Francis Giraldeau <francis.giraldeau@gmail.com>
+ *
+ */
+public class WeightedQuickUnion {
+
+    private int id[];
+    private int sz[];
+    private int count;
+
+    public WeightedQuickUnion(int n) {
+        count = n;
+        id = new int[n];
+        sz = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        int k = p;
+        while(k != id[k]) {
+            k = id[k];
+        }
+        return k;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) {
+            return;
+        }
+
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        count--;
+    }
+}
