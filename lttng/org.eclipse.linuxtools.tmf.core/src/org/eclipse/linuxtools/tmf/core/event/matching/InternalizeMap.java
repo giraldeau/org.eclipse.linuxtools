516e5e7be75f8c48419e3afcf9d23d5898ded8cd
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-08-01 14:48:24 -0400

    Add efficient union find algorithm
    The union find allows to detect when there is a valid synchronization
    between each nodes, when there is only 1 partitions.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/matching/InternalizeMap.java
@@ -0,0 +1,34 @@
+package org.eclipse.linuxtools.tmf.core.event.matching;
+
+import java.util.HashMap;
+
+/**
+ * Keep unique integer ID for an object. The ids are not reused.
+ *
+ * @author francis
+ *
+ */
+public class InternalizeMap {
+
+    HashMap<Object, Integer> map = new HashMap<>();
+    int nextId = 0;
+
+    public int get(Object obj) {
+        return map.get(obj);
+    }
+
+    public void put(Object obj) {
+        if (!map.containsKey(obj)) {
+            map.put(obj, nextId++);
+        }
+    }
+
+    public Object remove(Object obj) {
+        return map.remove(obj);
+    }
+
+    public int size() {
+        return nextId;
+    }
+
+}
