84eaf9a3c40788849aa55f83f97d208e540e4688
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-08-08 16:47:04 -0400

    Implement consumer/producer for TmfTraceReader
    Increase reading speed by using two threads, one for the producer and the other
    for the consumer of events.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- lttng/org.eclipse.linuxtools.tmf.analysis.graph.core/src/org/eclipse/linuxtools/tmf/analysis/graph/core/staging/reader/TmfTraceReader.java
+++ lttng/org.eclipse.linuxtools.tmf.analysis.graph.core/src/org/eclipse/linuxtools/tmf/analysis/graph/core/staging/reader/TmfTraceReader.java
@@ -1,27 +1,72 @@
 package org.eclipse.linuxtools.tmf.analysis.graph.core.staging.reader;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.TmfEvent;
 import org.eclipse.linuxtools.tmf.core.request.TmfEventRequest;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfContext;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
 
 public class TmfTraceReader {
 
-    public TmfTraceReader() {
-    }
+    private int count;
 
-    public void readTraceRaw(ITmfTrace trace, TmfEventRequest request) {
-        ITmfContext pos = trace.seekEvent(0L);
+    ITmfEvent endEvent = new TmfEvent();
+
+    public void readTraceRaw(final ITmfTrace trace, final TmfEventRequest request) {
+
+        final ArrayBlockingQueue<ITmfEvent> fQueue = new ArrayBlockingQueue<>(10000);
         request.handleStarted();
-        int count = 0;
-        while(!request.isCancelled()) {
-            ITmfEvent event = trace.getNext(pos);
-            if (event == null) {
-                break;
+        Runnable producer = new Runnable() {
+            @Override
+            public void run() {
+                ITmfContext pos = trace.seekEvent(0L);
+                count = 0;
+                try {
+                    while(!request.isCancelled()) {
+                        ITmfEvent event = trace.getNext(pos);
+                        if (event == null) {
+                            break;
+                        }
+                        count++;
+                        fQueue.put(event);
+                    }
+                    fQueue.put(endEvent);
+                } catch (InterruptedException e) {
+                    new RuntimeException(e);
+                }
             }
-            count++;
-            request.handleData(event);
+        };
+
+        Runnable consumer = new Runnable() {
+            @Override
+            public void run() {
+                while (!request.isCancelled()) {
+                    try {
+                        ITmfEvent event = fQueue.take();
+                        if (event == endEvent) {
+                            break;
+                        }
+                        request.handleData(event);
+                    } catch (InterruptedException e) {
+                        new RuntimeException(e);
+                    }
+                }
+            }
+        };
+
+        Thread prodThread = new Thread(producer);
+        Thread consThread = new Thread(consumer);
+        prodThread.start();
+        consThread.start();
+        try {
+            prodThread.join();
+            consThread.join();
+        } catch (InterruptedException e) {
+            new RuntimeException(e);
         }
+
         request.handleCompleted();
         System.out.println("\nevents reads: " + count);
     }
